80f119b76b2e6a24b5eb7f4dd1639dff838caa97
zhangli20 <zhangli20@kuaishou.com>
2024-09-16 11:27:42 +0800

    blaze: use case-insensitive comparing in processing parquet file schema

--- datafusion/datasource/src/schema_adapter.rs
+++ datafusion/datasource/src/schema_adapter.rs
@@ -276,7 +276,10 @@ impl SchemaAdapter for DefaultSchemaAdapter {
     /// Panics if index is not in range for the table schema
     fn map_column_index(&self, index: usize, file_schema: &Schema) -> Option<usize> {
         let field = self.projected_table_schema.field(index);
-        Some(file_schema.fields.find(field.name())?.0)
+        file_schema
+            .fields
+            .iter()
+            .position(|f| f.name().eq_ignore_ascii_case(field.name()))
     }
 
     /// Creates a `SchemaMapping` for casting or mapping the columns from the
