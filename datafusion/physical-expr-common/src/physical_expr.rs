d228d6a5fcc7b8d2137cdc72942070b067052f8f
zhangli20 <zhangli20@kuaishou.com>
2024-09-16 10:04:54 +0800

    blaze: make scatter() public

--- datafusion/physical-expr-common/src/physical_expr.rs
+++ datafusion/physical-expr-common/src/physical_expr.rs
@@ -21,7 +21,7 @@ use std::hash::{Hash, Hasher};
 use std::sync::Arc;
 
 use crate::expressions::column::Column;
-use crate::utils::scatter;
+pub use crate::utils::scatter;
 
 use arrow::array::BooleanArray;
 use arrow::compute::filter_record_batch;
